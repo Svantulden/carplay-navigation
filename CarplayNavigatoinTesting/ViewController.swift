91b37658f1011623d43f33c61826311a3118c588
Svantulden <sandervantulden@gmail.com>
2018-07-02 21:00:04 +0200

    Initial Commit

---
+++ CarplayNavigatoinTesting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarplayNavigatoinTesting
+//
+//  Created by Sander van Tulden on 02/07/2018.
+//  Copyright © 2018 Sander van Tulden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
