c05e9fe00e377bd0ef700c936ec8d0aee82e62a6
Svantulden <sandervantulden@gmail.com>
2018-07-03 22:47:03 +0200

    Added placeholder buttons, background view, comments

--- CarplayNavigationTesting/ViewControllers/CustomNavigationViewController.swift
+++ CarplayNavigationTesting/ViewControllers/CustomNavigationViewController.swift
@@ -1,28 +1,28 @@
 //
-//  CarplayViewController.swift
-//  CarplayNavigationTesting
+//  CustomNavigationViewController.swift
+//  CarplayNav
 //
-//  Created by Sander van Tulden on 02/07/2018.
+//  Created by Sander van Tulden on 03/07/2018.
 //  Copyright © 2018 Sander van Tulden. All rights reserved.
 //
 
 import Foundation
+import UIKit
 import CarPlay
 
-class CarplayViewController: UIViewController, CPInterfaceControllerDelegate {
+class CustomNavigationViewController: UIViewController {
     var mainView: UIView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
-            appDelegate.interfaceController?.delegate = self
-        }
-        
+        // Just an example of showing your own views in the VC that you are setting as root on the CPMapContentWindow
         if mainView == nil {
             mainView = Bundle.main.loadNibNamed("CarplayView", owner: self, options: nil)?.first as? UIView
-            mainView?.frame = view.frame
+            mainView?.frame = view.bounds
             if let mainViewUnwrapped = mainView {self.view.addSubview(mainViewUnwrapped)}
         }
     }
+    
+    
 }
